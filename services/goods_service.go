72470766675caa0ec32f9d54b7d2d6c2c0a323a8
leiwen <657106593@qq.com>
2021-06-05 00:31:40 +0800

    feat:通过go-bbs模拟客户端调用

--- services/goods_service.go
+++ services/goods_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"fmt"
 )
 
 type GoodsService struct {
@@ -9,7 +10,9 @@ type GoodsService struct {
 }
 
 func (this *GoodsService) GetGoodsStock(ctx context.Context, in *GoodsRequest) (*GoodsResponse, error)  {
-	return &GoodsResponse{GoodsStock:25},nil
+	fmt.Println("GoodsRequest:",in)
+	fmt.Println("GoodsID:",in.GoodsId)
+	return &GoodsResponse{GoodsStock: in.GoodsId*10},nil
 }
 
 
